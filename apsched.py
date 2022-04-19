ab728e8c449b4ba50d7ca6003d5e553a0d7f151f
Denis Polom <denis.polom@nubium.cz>
2022-04-19 22:39:08 +0200

    hey

--- apsched.py
+++ apsched.py
@@ -3,15 +3,21 @@ import pytz
 from datetime import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def job_function():
     names = ('anna','roman','janicka','roza')
     for i in names:
         print("Hello ", i )
+    with open('/home/dp/Downloads/readme.txt', 'w') as f:
+        f.write('Create a new text file!')
 
-sched = BlockingScheduler(timezone=pytz.timezone('Europe/Prague'))
+# sched = BlockingScheduler(timezone=pytz.timezone('Europe/Prague'))
+sched = BackgroundScheduler(timezone=pytz.timezone('Europe/Prague'))
 
 # Runs from Monday to Friday at 5:30 (am) until
 sched.add_job(job_function, 'cron', day_of_week='mon-fri', hour=15, minute=15)
-sched.add_job(job_function, 'cron', day_of_week='mon,tue,fri', hour=15, minute=32)
+sched.add_job(job_function, 'cron', day_of_week='mon,tue,fri', hour=19, minute=11)
+# sched.remove_all_jobs()
 sched.start()
+sched.print_jobs()
