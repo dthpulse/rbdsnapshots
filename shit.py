2e30a35aebccbb31175cf2d42c0f051a07019d8a
Denis Polom <denis.polom@nubium.cz>
2022-04-24 16:10:51 +0200

    upravy komentu

--- shit.py
+++ shit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# import json
 import sys
 import os
 import yaml
@@ -19,7 +18,9 @@ from apscheduler.executors.pool import ProcessPoolExecutor
 snapmanager_dir = '/var/lib/snapmanager'
 os.mkdir(snapmanager_dir)
 
-## apscheduler settings
+'''
+apscheduler settings
+'''
 def scheduler_conf():
     MYSQL_SNAP = {
         "url": "mysql+pymysql://localhost:3306/snapshot_schedule"
@@ -46,7 +47,9 @@ def scheduler_conf():
     except:
         print("scheduler is already running")
 
-## connect to OS
+'''
+connect to OS
+'''
 def connect_to_os():
     os_conn = novaclient.Client(version = 2,
         username = 'foremannub',
@@ -57,13 +60,17 @@ def connect_to_os():
         project_domain_id = 'b2571699c2044245ac79c60e7c6ff09d')
     return os_conn
 
-## connect to Ceph
+'''
+connect to Ceph
+'''
 def ceph_conn():
     cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
     cluster.connect()
     ioctx = cluster.open_ioctx('rbd')
 
-## using librbd we're connecting to the Ceph and creating snapshots
+'''
+using librbd we're connecting to the Ceph and creating snapshots
+'''
 def create_general_snap(general_scheduled_servers):
     ceph_conn()
     keep_copies = '5'
@@ -88,7 +95,9 @@ def create_general_snap(general_scheduled_servers):
     ioctx.close()
     cluster.shutdown()
 
-## using librbd we're connecting to the Ceph and creating snapshots
+'''
+using librbd we're connecting to the Ceph and creating snapshots
+'''
 def create_scheduled_snap(snap_sched, server_details):
     ceph_conn()
     for schedule, server in snap_sched.items():
@@ -120,12 +129,14 @@ def create_scheduled_snap(snap_sched, server_details):
     ioctx.close()
     cluster.shutdown()
 
-## read yaml - snapshot schedule settings
-## snap_sched.yml is file defined by admins
-## here we also checking if there is any update by backing up the old one 
-## and comparing it with new one. This function is scheduled with apscheduler to run regularly
-## we get snap_sched dict like {'7@mon-fri@2,4':'['server1', 'server2',...]'}
-## and the list of scheduled_servers
+'''
+read yaml - snapshot schedule settings
+snap_sched.yml is file defined by admins
+here we also checking if there is any update by backing up the old one 
+and comparing it with new one. This function is scheduled with apscheduler to run regularly
+we get snap_sched dict like {'7@mon-fri@2,4':'['server1', 'server2',...]'}
+and the list of scheduled_servers
+'''
 def snap_sched():
     orig_yaml = snapmanager_dir + '/snap_sched.yml'
     used_yaml = orig_yaml + '-'
@@ -151,10 +162,12 @@ def snap_sched():
 
     return snap_sched, scheduled_servers
 
-## get server list with IDs and volumes IDs from OpenStack
-## here we also checking if there is any update on OS by backing up the server list 
-## and comparing it with new one. This function is scheduled with apscheduler to run regularly
-## we get dict with {'server_name':'[volume1, volume2, ...]'}
+'''
+get server list with IDs and volumes IDs from OpenStack
+here we also checking if there is any update on OS by backing up the server list 
+and comparing it with new one. This function is scheduled with apscheduler to run regularly
+we get dict with {'server_name':'[volume1, volume2, ...]'}
+'''
 def openstack_server_list(os_conn):
     server_details = dict()
     servers_list = os_conn.servers.list()
@@ -189,7 +202,9 @@ def openstack_server_list(os_conn):
     
     return server_details
 
-## VMs not scheduled in yaml file will be backed up with general schedule
+'''
+VMs not scheduled in yaml file will be backed up with general schedule
+'''
 def not_defined_servers(scheduled_servers, server_details):
     general_scheduled_servers = {}
     for server_name,server_volumes in server_details.items():
@@ -197,7 +212,11 @@ def not_defined_servers(scheduled_servers, server_details):
             general_scheduled_servers[server_name] = server_volumes
     return general_scheduled_servers
 
-def create_general_schedule_job(general_scheduled_servers):
+'''
+mel by volat funkci create_general_snap - ale potrebuji ji jeste upravit,
+aby nevyzadovala zadny argument
+'''
+def create_general_snap_job(general_scheduled_servers):
     hour = '6,12,18'
     day_of_week = 'mon-fri'
     scheduler_conf()
@@ -225,7 +244,8 @@ def create_service_schedule_job():
         minute='25', 
         jobstore='mysql_service', 
         replace_existing=True, 
-        id='snap_sched')
+        id='snap_sched',
+        misfire_grace_time=600)
     scheduler.add_job(
         server_list, 
         'cron', 
